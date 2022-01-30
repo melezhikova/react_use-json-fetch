8ec6c6604b4c4e4b41312086b58dd772f73a6bac
Liudmila Melezhikova <melezhikova@yandex.ru>
2022-01-30 20:18:22 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { Fragment } from 'react/cjs/react.production.min';
 import './App.css';
+import useJsonFetch from './useJsonFetch';
+
+function Comp1 () {
+  const [data, loading, error] = useJsonFetch(
+    "http://localhost:7070/", 
+    "data"
+  );
+  return (
+    <Fragment>
+      {data && <div>{data.status}</div>}
+      {loading && <p>Loading...</p>}
+      {error && <p>Ups... something wrong:(</p>}
+    </Fragment>
+  )
+}
+
+function Comp2 () {
+  const [data, loading, error] = useJsonFetch(
+    "http://localhost:7070/", 
+    "error"
+  );
+  return (
+    <Fragment>
+      {data && <div>{data.status}</div>}
+      {loading && <p>Loading...</p>}
+      {error && <p>Ups... something wrong:(</p>}
+    </Fragment>
+  )
+}
+
+function Comp3 () {
+  const [data, loading, error] = useJsonFetch(
+    "http://localhost:7070/", 
+    "loading"
+  );
+  return (
+    <Fragment>
+      {data && <div>{data.status}</div>}
+      {loading && <p>Loading...</p>}
+      {error && <p>Ups... something wrong:(</p>}
+    </Fragment>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Comp1 />
+      <Comp2 />
+      <Comp3 />
     </div>
   );
 }
---
+++ src/useJsonFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+export default function useJsonFetch (url, opts) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            setLoading(true);
+            try {
+                const response = await fetch(`${url}${opts}`);
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                const data = await response.json();
+                setData(data);
+                setError(null);
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+
+    return [data, loading, error];
+}
